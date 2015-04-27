b28c41db08fe179249dc0c6eee7c4699bb7aa6c2
Roman Kozachenko <roman@alt-soft.com>
2015-04-27 14:41:40 +0300

    Added IShopifyConverter

---
+++ Altsoft.ShopifyImportModule.Data/Interfaces/IShopifyConverter.cs
@@ -0,0 +1,14 @@
+﻿using Altsoft.ShopifyImportModule.Data.Models;
+using VirtoCommerce.Foundation.Catalogs.Model;
+
+namespace Altsoft.ShopifyImportModule.Data.Interfaces
+{
+    public interface IShopifyConverter
+    {
+        //ShopifyProduct Convert(catalogProductEntity product);
+        Product Convert(ShopifyProduct product);
+
+        //ShopifyCategory Convert(catalogCategoryEntity category);
+        Category Convert(ShopifyCategory category);
+    }
+}
