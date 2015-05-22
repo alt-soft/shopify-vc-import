9f352943c4b083a6658be2d9e03e90e0e59d7438
Roman Kozachenko <roman@alt-soft.com>
2015-05-22 17:27:33 +0300

    Checking/Unchecking products

--- Altsoft.ShopifyImportModule.Web/Scripts/blades/shopify-product-list.js
+++ Altsoft.ShopifyImportModule.Web/Scripts/blades/shopify-product-list.js
@@ -1,11 +1,18 @@
 ﻿angular.module('altsoft.shopifyImportModule')
 .controller('shopifyProductListController', ['$scope', 'shopifyImportResources', 'platformWebApp.bladeNavigationService', function ($scope, shopifyImportResources, bladeNavigationService) {
 
+    function closeChildrenBlades() {
+        angular.forEach($scope.blade.childrenBlades.slice(), function (child) {
+            bladeNavigationService.closeBlade(child);
+        });
+    }
+
     $scope.blade.refresh = function() {
         if ($scope.blade.products) {
             $scope.products = $scope.blade.products;
             $scope.blade.isLoading = false;
         } else {
+            closeChildrenBlades();
             $scope.blade.isLoading = true;
             shopifyImportResources.getCollections({}, function(result) {
                 $scope.products = result;
@@ -14,11 +21,16 @@
         }
     };
 
-    function closeChildrenBlades() {
-        angular.forEach($scope.blade.childrenBlades.slice(), function (child) {
-            bladeNavigationService.closeBlade(child);
-        });
+   
+
+    var selectChildren = function (product) {
+        if (product.children && product.children.length > 0) {
+            $(product.children).each(function (index, child) {
+                child.isSelected = product.isSelected;
+            });
+        }
     }
+
 
     $scope.showChildren = function (product) {
         if (product.isCollection && product.children) {
@@ -27,6 +39,7 @@
                 id: 'shopify-collection-items',
                 products: product.children,
                 title: product.title,
+                parentCollection: product,
                 template: 'Modules/$(Altsoft.ShopifyImport)/Scripts/blades/shopify-product-list.tpl.html',
                 isClosingDisabled: false,
                 isLoading: false,
@@ -35,19 +48,38 @@
             bladeNavigationService.showBlade(blade);
         }
     }
+    var allProductsChecked = function (products) {
+        var result = true;
+        $(products).each(function (index, product) {
+            if (!product.isSelected)
+                result = false;
+        });
 
-    $scope.bladeToolbarCommands = [
-        {
-            name: "Refresh",
-            icon: 'fa fa-refresh',
-            executeMethod: function () {
-                $scope.blade.refresh();
-            },
-            canExecuteMethod: function () {
-                return true;
-            }
+        return result;
+    }
+    
+   
+
+    $scope.changeSelection = function(product) {
+        if ($scope.blade.parentCollection) {
+            $scope.blade.parentCollection.isSelected = allProductsChecked($scope.blade.parentCollection.children);
         }
-    ];
+
+        selectChildren(product);
+    }
+
+        $scope.bladeToolbarCommands = [
+            {
+                name: "Refresh",
+                icon: 'fa fa-refresh',
+                executeMethod: function() {
+                    $scope.blade.refresh();
+                },
+                canExecuteMethod: function() {
+                    return !$scope.blade.products;
+                }
+            }
+        ];
 
     $scope.blade.refresh();
 }]);
