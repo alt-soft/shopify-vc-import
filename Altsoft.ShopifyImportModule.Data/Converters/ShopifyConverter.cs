cc591b2753ba45e9a6340deb84189d680052e5ba
Roman Kozachenko <roman@alt-soft.com>
2015-06-17 15:39:45 +0300

    Product images import

--- Altsoft.ShopifyImportModule.Data/Converters/ShopifyConverter.cs
+++ Altsoft.ShopifyImportModule.Data/Converters/ShopifyConverter.cs
@@ -1,8 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using Altsoft.ShopifyImportModule.Data.Interfaces;
 using Altsoft.ShopifyImportModule.Data.Models;
 using Altsoft.ShopifyImportModule.Data.Models.Shopify;
 using VirtoCommerce.CatalogModule.Data.Model;
+using VirtoCommerce.Domain.Catalog.Model;
+using VirtoCommerce.Platform.Core.Common;
+using ItemAsset = VirtoCommerce.CatalogModule.Data.Model.ItemAsset;
 
 namespace Altsoft.ShopifyImportModule.Data.Converters
 {
@@ -17,7 +23,29 @@ namespace Altsoft.ShopifyImportModule.Data.Converters
             result.StartDate = product.PublishedAt;
             result.IsActive = true;
 
+            if (product.Images != null)
+            {
+                result.ItemAssets = new ObservableCollection<ItemAsset>();
+                result.ItemAssets.AddRange(product.Images.Select(image => Convert(image, result, false)));
+            }
+
             return result;
+        }
+
+        private ItemAsset Convert(ShopifyImage image, Item item, bool isMain)
+        {
+            var retVal = new ItemAsset
+            {
+                CreatedDate = image.CreatedAt,
+                AssetType = ItemAssetType.Image.ToString().ToLower(),
+                CatalogItem = item,
+                ItemId = item.Id,
+                ModifiedDate = image.UpdatedAt,
+                GroupName = isMain ? "primaryimage" : "images",
+                AssetId = image.Src
+            };
+
+            return retVal;
         }
 
         public VirtoCategory Convert(ShopifyCustomCollection category)
